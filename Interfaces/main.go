22ee336e2d35b5cf7acfd337dd4a9bbe81608b4f
atmikshetty <atmikshetty10@student.sfit.ac.in>
2024-10-06 14:51:42 +0530

    Interfaces

--- Interfaces/main.go
+++ Interfaces/main.go
@@ -2,6 +2,42 @@ package main
 
 import "fmt"
 
+// Interfaces is a type which defines a set of methods other types must implement, it does not proivde any implementation, i.e. left to the other methods
+
+type football interface {
+	Goat() string
+}
+
+type Messi struct {
+}
+
+type Anthony struct {
+}
+
+func (m Messi) Goat() string {
+	return "Goat No 2."
+}
+
+func (a Anthony) Goat() string {
+	return "Goat No 1."
+}
+
+func printVals(val interface{}) {
+	fmt.Println(val)
+}
+
 func main() {
-	fmt.Println("Testing...")
+	var footballer football
+	var footballer2 football
+
+	footballer = Messi{}
+	footballer2 = Anthony{}
+
+	fmt.Println(footballer.Goat())
+	fmt.Println(footballer2.Goat())
+
+	//	Empty Interfaces
+	printVals(10)
+	printVals("Atmik Shetty")
+	printVals(99.9999999)
 }
